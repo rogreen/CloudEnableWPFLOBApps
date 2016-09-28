a3f07d0042b3d55d91c8a9d34f0216b718966a96
Robert Green <rogreen@microsoft.com>
2016-09-28 08:10:33 -0700

    Examples 2 - 5 uploaded

---
+++ Expenses - 05 After Service Bus/Expenses.InternalService/InternalService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Expenses.InternalService
+{
+    public class InternalService : IInternalService
+    {
+        public void ProcessReport(int expenseReportId)
+        {
+            //TODO: Put code in here to do something useful
+        }
+    }
+}
